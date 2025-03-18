5a2b4945af110d1107804401db45a2ad983eb733
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-03-18 13:47:28 +0100

    Basic structure

---
+++ WebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class ProjectsController : Controller
+{
+    [Route("projects")]
+    public IActionResult Projects()
+    {
+        return View();
+    }
+}
