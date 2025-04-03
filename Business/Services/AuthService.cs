b71433cc54b05507981fd4020cb12ff8bd9d750b
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-04-03 17:39:50 +0200

    Write Auth service method and linker with auth controller.

---
+++ Business/Services/AuthService.cs
@@ -0,0 +1,22 @@
+﻿
+using Data.Entities;
+using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Business.Services;
+
+public interface IAuthService
+{
+    Task<bool> LoginAsync(MemberLoginForm loginForm);
+}
+
+public class AuthService(SignInManager<MemberEntity> signInManager) : IAuthService
+{
+    private readonly SignInManager<MemberEntity> _signInManager = signInManager;
+
+    public async Task<bool> LoginAsync(MemberLoginForm loginForm)
+    {
+        var result = await _signInManager.PasswordSignInAsync(loginForm.Email, loginForm.Password, false, false);
+        return result.Succeeded;
+    }
+}
--- Domin/Models/MemberLoginForm.cs
+++ Domin/Models/MemberLoginForm.cs
@@ -5,14 +5,14 @@ namespace Domain.Models;
 
 public class MemberLoginForm
 {
-
+    [Required]
     [Display(Name = "Email", Prompt = "Enter email address")]
     [DataType(DataType.EmailAddress)]
-    public string? Email { get; set; }
+    public string Email { get; set; } = null!;
 
-
+    [Required]
     [Display(Name = "Password", Prompt = "Enter Password")]
     [DataType(DataType.Password)]
-    public string? Password { get; set; }
+    public string Password { get; set; } = null!;
 
 }
