def81a6fcf9796c5e682bbaaffea567ced920540
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-04-03 16:30:53 +0200

    Add Member Login Form in domain project.

---
+++ Domin/Models/MemberLoginForm.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
+
+public class MemberLoginForm
+{
+    [Required]
+    [Display(Name = "Email", Prompt = "Enter email address")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [Display(Name = "Password", Prompt = "Enter Password")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+}
