97c1f1835e044db462c8aeb748e5a5588a7f85c0
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-04-05 14:08:54 +0200

    Create Member SignUp Form.

--- WebApp/Controllers/AuthController.cs
+++ WebApp/Controllers/AuthController.cs
@@ -8,32 +8,46 @@ namespace WebApp.Controllers;
 public class AuthController(IAuthService authService) : Controller
 {
     private readonly IAuthService _authService = authService;
-    public IActionResult Register()
-    {
+
+    #region
+    public IActionResult SignUp()
+    {       
+        ViewBag.ErrorMessage = "";
         return View();
     }
+    [HttpPost]
+    public async Task<IActionResult> SignUp(MemberSignUpForm form)
+    {
+        ViewBag.ErrorMessage = "";
+
+       
+        return View(form);
+    }
+    #endregion
+
+    #region login
     public IActionResult Login()
     {
         //return LocalRedirect("/projects");
-        ViewBag.ErrorMessage = null;
+        ViewBag.ErrorMessage = "";
         return View();
     }
     [HttpPost]
     public async Task<IActionResult> Login(MemberLoginForm form, string returnUrl = "~/")
     {
-        ViewBag.ErrorMessage = null;
+        ViewBag.ErrorMessage = "";
 
-        if (!ModelState.IsValid)
+        if (ModelState.IsValid)
         {
+            var result = await _authService.LoginAsync(form);
+            if (result)
+            {
+                return Redirect(returnUrl);
+            }
+        }
+
             ViewBag.ErrorMessage = "Incorrect email or password.";
             return View(form);
-        }
-        var result = await _authService.LoginAsync(form);
-        if (result)
-        {
-           return Redirect(returnUrl);
-        }
-            ViewBag.ErrorMessage = "Unable to login now. Please try again later.";
-            return View(form);
     }
+#endregion
 }
