57008161e0ea696884430d12bee91b951ed31dc0
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-04-08 16:08:45 +0200

    Update data base with user entity.

---
+++ Data/Entities/ClientEntity.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+
+public class ClientEntity
+{
+    [Key]
+    public string Id { get; set; } = null!;
+
+    [Index(nameof(ClientName), IsUnique = true)]
+    public string ClientName { get; set; } = null!;
+    public virtual ICollection<ProjectEntity> Projects { get; set; } = [];
+}
--- WebApp/Controllers/ProjectsController.cs
+++ WebApp/Controllers/ProjectsController.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApp.Controllers;
 
 [Authorize]
+
 public class ProjectsController : Controller
 {
- 
     [Route("projects")]
     public IActionResult Projects()
     {
