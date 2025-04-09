8de8cf6902ab71f763d2137ac1a45ea23ffaa30b
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-04-09 08:31:53 +0200

    Map to TModel i base repository.

--- Data/Repositories/BaseRepository.cs
+++ Data/Repositories/BaseRepository.cs
@@ -1,21 +1,22 @@
 ﻿using System.Diagnostics;
 using System.Linq.Expressions;
 using Data.Context;
+using Domain.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories;
 
-public interface IBaseRepository<TEntity> where TEntity : class
+public interface IBaseRepository<TEntity, TModel> where TEntity : class
 {
     Task<bool> AddAsync(TEntity entity);
-    Task<bool> DeleteAsync(TEntity entity);
+    Task<IEnumerable<TSelect>> GetAllAsync<TSelect>(Expression<Func<TEntity, TSelect>> selector, bool orderByDescending = false, Expression<Func<TEntity, object>>? storBy = null, Expression<Func<TEntity, bool>>? where = null, params Expression<Func<TEntity, object>>[] includes);
+    Task<TModel> GetAsync(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] includes);
     Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> findBy);
-    Task<IEnumerable<TEntity>> GetAllAsync();
-    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> findBy);
     Task<bool> UpdateAsync(TEntity entity);
+ Task<bool> DeleteAsync(TEntity entity);
 }
 
-public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+public abstract class BaseRepository<TEntity, TModel> : IBaseRepository<TEntity, TModel> where TEntity : class
 {
 
     protected readonly DataContext _context;
@@ -46,15 +47,53 @@ public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where T
         }
     }
     //READ
-    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+    public virtual async Task<IEnumerable<TModel>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? storBy = null, Expression<Func<TEntity, bool>>? where = null, params Expression<Func<TEntity, object>>[] includes)
     {
-        var entities = await _table.ToListAsync();
-        return entities;
+        IQueryable<TEntity> query = _table;
+        if (where != null)
+            query = query.Where(where);
+        if (includes != null && includes.Length != 0)
+            foreach (var include in includes)
+                query = query.Include(include);
+        if (storBy != null)
+            query = orderByDescending
+                ? query.OrderByDescending(storBy)
+                : query.OrderBy(storBy);
+
+        var entities = await query.ToListAsync();
+        var result = entities.Select(entity => entity.MapTo<TModel>());
+        return result;
     }
-    public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> findBy)
+    public virtual async Task<IEnumerable<TSelect>> GetAllAsync<TSelect>(Expression<Func<TEntity, TSelect>> selector ,bool orderByDescending = false, Expression<Func<TEntity, object>>? storBy = null, Expression<Func<TEntity, bool>>? where = null, params Expression<Func<TEntity, object>>[] includes)
     {
-        var entity = await _table.FirstOrDefaultAsync(findBy);
-        return entity ?? null!;
+        IQueryable<TEntity> query = _table;
+        if (where != null)
+            query = query.Where(where);
+        if (includes != null && includes.Length != 0)
+            foreach (var include in includes)
+                query = query.Include(include);
+        if (storBy != null)
+            query = orderByDescending
+                ? query.OrderByDescending(storBy)
+                : query.OrderBy(storBy);
+
+        var entities = await query.Select(selector).ToListAsync();
+        var result = entities.Select(entity => entity!.MapTo<TSelect>());
+        return result;
+    }
+    public virtual async Task<TModel> GetAsync(Expression<Func<TEntity, bool>> where, params Expression<Func<TEntity, object>>[] includes)
+    {
+        IQueryable<TEntity> query = _table;
+        if (includes != null && includes.Length != 0)
+            foreach (var include in includes)
+                query = query.Include(include);
+
+
+        var entity = await query.FirstOrDefaultAsync(where);
+        if (entity == null)
+            throw new Exception("Entity not found");
+        var result = entity.MapTo<TModel>();
+        return result;
     }
     public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> findBy)
     {
@@ -96,6 +135,5 @@ public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where T
             return false;
         }
     }
-
-    #endregion
+#endregion
 }
