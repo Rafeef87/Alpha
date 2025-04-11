b78c0b39596e70c4446678078f69dadba7db3b14
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-04-11 09:27:49 +0200

    User controller with skydd and standar roll.

---
+++ WebApp/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+[Authorize]
+public class UsersController(RoleManager<IdentityRole> roleManager) : Controller
+{
+    private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+    public async Task<IActionResult> Index()
+    {
+        ViewBag.Roles = await _roleManager.Roles.Select(x => new SelectListItem {  Value = x.Name, Text = x.Name}).ToListAsync();
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(UserRegistrationViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Roles = await _roleManager.Roles.Select(x => new SelectListItem { Value = x.Name, Text = x.Name }).ToListAsync();
+            return View(model);
+        }
+        return RedirectToAction("Index");
+    }
+}
